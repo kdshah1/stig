d9426e39b1fd5504661ee1eaee598fc4d5e92f18
Kina Shah <KDSHAH@MITRE.ORG>
2018-05-09 12:48:26 -0400

    Added check for os version to execute this recipe.

--- recipes/inittab.rb
+++ recipes/inittab.rb
@@ -5,9 +5,14 @@
 #
 # Description: This cookbook password protects Linux single user mode.
 
+platform = node['platform']
+major_version = node['platform_version'][0, 1].to_i
+
 template '/etc/inittab' do
   source  'etc_inittab.erb'
   owner   'root'
   group   'root'
   mode    '0644'
+  only_if { %w[rhel fedora centos redhat].include? platform }
+  only_if { major_version < 7 }
 end
